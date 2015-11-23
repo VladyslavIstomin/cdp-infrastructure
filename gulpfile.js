235b4a18eafdea9b0e5efc055b123bcb1afedfc6
Vladyslav_Istomin <Vladyslav_Istomin@epam.com>
2015-11-23 13:32:16 +0200

    restructure the project

--- gulpfile.js
+++ gulpfile.js
@@ -8,73 +8,58 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var copy = require('gulp-copy');
 
-var conf = {
+var config = {
+    core: {
+        folder: 'core',
+        style: 'core/less/**/*.less',
+        js: 'core/js/**/*.js',
+        test: '!core/js/**/*.test.js'
+    },
     build: {
         folder: 'build',
-        css: 'build/css',
+        style: 'build/css',
         js: 'build/js'
+    },
+    bootstrap: {
+        style: 'bower_components/bootstrap/less/bootstrap.less',
+        js: 'bower_components/bootstrap/js/*.js'
     }
-};
-var bootstrap = {
-    less: 'bower_components/bootstrap/less/*.less',
-    js: 'bower_components/bootstrap/js/*.js'
 };
 
 gulp.task('jscs', function() {
-    return gulp.src('all.js')
+    return gulp.src(config.core.js)
         .pipe(jscs())
         .pipe(jscs.reporter());
 });
 
 gulp.task('jshint', function() {
-    return gulp.src('all.js')
+    return gulp.src(config.core.js)
         .pipe(jshint())
         .pipe(jshint.reporter());
 });
 
 gulp.task('css', function () {
-    return gulp.src(['all.less', bootstrap.less])
+    return gulp.src([config.core.style, config.bootstrap.style])
         .pipe(less())
         .pipe(concat('all.css'))
         .pipe(csso())
         .on('error', errorHandler)
-        .pipe(gulp.dest(conf.build.css));
+        .pipe(gulp.dest(config.build.style));
 });
 
 gulp.task('js', function () {
-    return gulp.src(['all.js', bootstrap.js])
+    return gulp.src([config.bootstrap.js, config.core.js, config.core.test])
         .pipe(concat('all.js'))
         .pipe(uglify())
         .on('error', errorHandler)
-        .pipe(gulp.dest(conf.build.js));
+        .pipe(gulp.dest(config.build.js));
 });
 
 gulp.task('html', function() {
     return gulp.src('index.html')
-        .pipe(copy(conf.build.folder, {prefix:0}));
+        .pipe(copy(config.build.folder, {prefix:0}));
 });
 
-
-
-gulp.task('style-build', ['bower', 'sprite'], function () {
-    return gulp.src([conf.less, bootstrap.less])
-        .pipe(less())
-        .pipe(autoprefixer(['last 2 version']))
-
-        .pipe(gulp.dest(conf.release.css));
-});
-
-gulp.task('clean', function (cb) {
-    del([conf.release.folder, conf.build.folder], cb);
-});
-
-gulp.task('script-build', ['bower'], function () {
-    return gulp.src(mainBowerFiles({includeDev: true}))
-        .pipe(filter('*.js'))
-        .pipe(concat('cdp.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(conf.release.js));
-});
 
 function errorHandler(err){
     util.log(util.colors.red('Error'), err.message);
